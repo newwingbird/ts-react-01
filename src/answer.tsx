1529bad1801e8a76687962a9d4436a66b9cec92d
newwingbird <newwingbird@icloud.com>
2022-03-23 05:42:11 +0900

    css

--- src/answer.tsx
+++ src/answer.tsx
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 
-const App = () => {
-  const [todos, setTodos] = useState<Todo[]>([]);
+function App() {
   const [inputText, setInputText] = useState("");
   //todo配列オブジェクトの更新用に用意。プロパティはinputValue, id, checkedの３つを更新する。
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   type Todo = {
     inputValue: string;
@@ -13,40 +13,79 @@ const App = () => {
     checked: boolean;
   };
 
-  // eの方は暗黙の型になってしまうのでfixed 機能で直してあげる
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
+
     setInputText(e.target.value);
-    console.log(inputText);
   };
 
   const handleSubmit = (e: { preventDefault: () => void }) => {
     e.preventDefault();
+
     if (!inputText) {
       return;
     }
 
-    // 新しいTodoを作成 以前作った型を指定してあげる
+    //新しいTodo作成
     const newTodo: Todo = {
       inputValue: inputText,
       id: todos.length,
       checked: false,
     };
+
     setTodos([newTodo, ...todos]);
     console.log(inputText);
     setInputText(inputText);
   };
 
+  //todo編集
   const handleEdit = (id: number, inputValue: string) => {
-    const newTodos = todos.map((todo) => {
+    /* ディープコピー(完全にコピーされた別の配列)に変えよう(ミューテートから守るため) */
+    const deepCopy = todos.map((todo) => ({ ...todo }));
+    console.log(deepCopy);
+
+    const newTodos = deepCopy.map((todo) => {
       if (todo.id === id) {
         todo.inputValue = inputValue;
       }
       return todo;
     });
-    // const の左辺右辺の型が等しくないのでエラーが吐かれていた
+
+    // /* シャローコピー */
+    // const newTodos = todos.map((todo) => {
+    //   if (todo.id === id) {
+    //     todo.inputValue = inputValue;
+    //   }
+    //   return todo;
+    // });
+
     setTodos(newTodos);
   };
+
+  //完了未完了
+  const handleChecked = (id: number, checked: boolean) => {
+    /* ディープコピー(完全にコピーされた別の配列)に変えよう(ミューテートから守るため) */
+    const deepCopy = todos.map((todo) => ({ ...todo }));
+    // console.log(deepCopy);
+
+    const newTodos = deepCopy.map((todo) => {
+      if (todo.id === id) {
+        //反転
+        todo.checked = !checked;
+      }
+      return todo;
+    });
+
+    setTodos(newTodos);
+  };
+
+  //削除
+  const handleDelete = (id: number) => {
+    //idが正しくないのは残す。正しいと消す。
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  };
+
   return (
     <div className="App">
       <div>
@@ -70,18 +109,18 @@ const App = () => {
                 onChange={(e) => handleEdit(todo.id, e.target.value)}
                 disabled={todo.checked}
               />
-              {/* <input
+              <input
                 type="checkbox"
                 checked={todo.checked}
                 onChange={() => handleChecked(todo.id, todo.checked)}
               />
-              <button onClick={() => handleDelete(todo.id)}>消</button> */}
+              <button onClick={() => handleDelete(todo.id)}>消</button>
             </li>
           ))}
         </ul>
       </div>
     </div>
   );
-};
+}
 
 export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import { Twitter } from "@mui/icons-material";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="icon">
+      <Twitter color="primary" fontSize="large" />
+    </div>
+  );
+};
+
+export default Header;
